97a0e4078de28057455514e1f0eaf3a6ec01277e
Triggs Gabriel <triggsg@appstate.edu>
2013-04-24 11:49:02 -0400

    Optimized precomputed cache fitness

--- FitnessPCC.cu
+++ FitnessPCC.cu
@@ -17,17 +17,13 @@
 #define KC2 10
 
 //fuction declarations
-int Choose(int, int);
 int EvaluateEdges(int[]);
 void GetAdjacencyMatrixFromCharArray(char[], int[N][N]);
-void GetEdgePossibilities(int[KC2][2]);
 void GetElement(int, int[]);
-void GetElement(int, int, int, int[]);
 int GetLargestV(int, int, int);
 
 //global variables
-int edges[KC2][2];
-std::string bit_string = "011001110000011100100001110110000000010100110010010111110111010100111111110100011001+000010011001111100101011111000001010011101101110100101011001100110001101101000000011000010110100011111001110100010101011001110010001110000111101000101010111100100000101110111101101011010000011000110001101001110110110111001011001101011110100011111011010001100010100010100101011110101100010001100101111011011000010110000101001001010101000101110110111110011101100001000100011111011101001010101111010101110011010001111000110111010011011001001011001100000000100110010111111100010010001011010110011010101001110010100111001001011100100100100100010110011110000100101010111110101101000001111001111101100011111001010101000010011001110100100011100101011000011100010101110011101111000101000110001010100100111100101111011010100001100010010101011000100010101110101010111101001110000110110101001000010011110111001100101111011100001010";
+std::string bit_string = "011001110000011100100001110110000000010100110010010111110111010100111111110100011001000010011001111100101011111000001010011101101110100101011001100110001101101000000011000010110100011111001110100010101011001110010001110000111101000101010111100100000101110111101101011010000011000110001101001110110110111001011001101011110100011111011010001100010100010100101011110101100010001100101111011011000010110000101001001010101000101110110111110011101100001000100011111011101001010101111010101110011010001111000110111010011011001001011001100000000100110010111111100010010001011010110011010101001110010100111001001011100100100100100010110011110000100101010111110101101000001111001111101100011111001010101000010011001110100100011100101011000011100010101110011101111000101000110001010100100111100101111011010100001100010010101011000100010101110101010111101001110000110110101001000010011110111001100101111011100001010";
 char *char_bits = new char[bit_string.size()];
 int adjacency_matrix[N][N];
 int choose_cache[][6] = {
@@ -89,20 +85,18 @@ int main(int argc, char *argv[])
         char_bits[i] = (bit_string[i] == '0') ? 0 : 1;
     }
 
-    GetEdgePossibilities(edges);
     GetAdjacencyMatrixFromCharArray(char_bits, adjacency_matrix);
 
     srand (time(NULL)); // init random seed
-    int upper_bound = Choose(N, K);
+    int upper_bound = choose_cache[N][K];
     std::cout << upper_bound << std::endl;
     int arr[5] = { 0, 0, 0, 0, 0 };
 
     // need to figure out time stuff
-    //long total_time = 0;
-    //long first_time = time(NULL); // get current time
-    //long time_limit = 5;
-    //print();
-
+    long total_time = 0;
+    long first_time = time(NULL); // get current time
+    long time_limit = 5;
+    int count = 0;
     do {
         int num_cliques = 0;
 
@@ -118,8 +112,8 @@ int main(int argc, char *argv[])
             }
         }
 
-        //long current_time = time(NULL);
-        //total_time = current_time - first_time;
+        long current_time = time(NULL);
+        total_time = current_time - first_time;
         
         std::cout << "Cliques: " << num_cliques << std::endl;
 
@@ -135,48 +129,11 @@ int main(int argc, char *argv[])
         int x = rand() % bit_string.size(); // random number over [0, char_bits.length)
         char_bits[x] = !char_bits[x];
         GetAdjacencyMatrixFromCharArray(char_bits, adjacency_matrix);
-    } while (1); //FIX THIS FIX IT FIX ITASDFGFJASDFIFAJSDIFJASWDIFJASDIFJASCIFAJSIFAJSDKIFAHJSDIFNHJASDIFNHASDIFNHASDIFHASDIHFASKIDNHFASDKINHF
-}
-
-/*
- * Returns nCk.
- */
-int Choose(int n, int k)
-{
-    return choose_cache[n][k];
-//  if (n < k) {
-//      return 0;
-//  } else if (n == k) {
-//      return 1;
-//  }
-//
-//  /* check to see whether it's already cached */
-//  if (choose_cache[n-1][k-1] != 0) {
-//      return choose_cache[n-1][k-1];
-//  }
-//
-//  /* take advantage of the fact that nCk == nC(n-k) to do faster computation */
-//  int diff;
-//  int max;
-//
-//  if (k < (n - k)) {
-//      diff = n - k;
-//      max = k;
-//  } else {
-//      diff = k;
-//      max = n - k;
-//  }
-//
-//  int ans = diff + 1;
-//
-//  for (int i = 2; i <= max; i++) {
-//      ans = (ans * (diff + i)) / i;
-//  }
-//
-//  /* cache answer before returning */
-//  choose_cache[n-1][k-1] = ans;
-//
-//  return ans;
+        count++;
+    } while (total_time < 5);
+    std::cout << "Number of graphs processed: " << count << std::endl;
+    std::cout << "Time per graph: " << total_time / (float)count << std::endl;
+    std::getchar();
 }
 
 /*
@@ -187,25 +144,16 @@ int Choose(int n, int k)
  */
 int EvaluateEdges(int arr[])
 {
-    int result = 0;
-
-    /*for (int i = 0; i < KC2; i++) {
-        result += adjacency_matrix[arr[edges[i][0]]][arr[edges[i][1]]];
-    }*/
-
-    /* Unrolled version for K == 5 */
-    result = adjacency_matrix[arr[0]][arr[1]] +
-              adjacency_matrix[arr[0]][arr[2]] +
-              adjacency_matrix[arr[0]][arr[3]] +
-              adjacency_matrix[arr[0]][arr[4]] +
-              adjacency_matrix[arr[1]][arr[2]] +
-              adjacency_matrix[arr[1]][arr[3]] +
-              adjacency_matrix[arr[1]][arr[4]] +
-              adjacency_matrix[arr[2]][arr[3]] +
-              adjacency_matrix[arr[2]][arr[4]] +
-              adjacency_matrix[arr[3]][arr[4]];
-
-    return result;
+    return adjacency_matrix[arr[0]][arr[1]] +
+           adjacency_matrix[arr[0]][arr[2]] +
+           adjacency_matrix[arr[0]][arr[3]] +
+           adjacency_matrix[arr[0]][arr[4]] +
+           adjacency_matrix[arr[1]][arr[2]] +
+           adjacency_matrix[arr[1]][arr[3]] +
+           adjacency_matrix[arr[1]][arr[4]] +
+           adjacency_matrix[arr[2]][arr[3]] +
+           adjacency_matrix[arr[2]][arr[4]] +
+           adjacency_matrix[arr[3]][arr[4]];
 }
 
 /*
@@ -221,27 +169,6 @@ void GetAdjacencyMatrixFromCharArray(char bit_arr[], int adj[N][N])
             adj[j][i] = bit_arr[x];
             x++;
         }
-    }
-}
-
-/*
- * Grab an array of edges to check for cliques. These are represented 
- * as a tuple of indices into the array returned by getElement(m), e.g.:
- * 
- * {
- *  {0, 1},
- *  {0, 2},
- *  {1, 2}
- * }
- * 
- * For k = 3
- * 
- */
-void GetEdgePossibilities(int arr[KC2][2])
-{
-    int num_edges = KC2;
-    for (int i = 0; i < num_edges; i++) {
-        GetElement(i, K, 2, arr[i]);
     }
 }
 
@@ -267,28 +194,6 @@ void GetElement(int m, int arr[])
 }
 
 /*
- * Same as above, but allows specification of n and k.
- * Used by GetEdgePossibilities().
- */
-void GetElement(int m, int n, int k, int arr[]) 
-{
-    int a = n;
-    int b = k;
-    int x = (Choose(n, k) - 1) - m; // x is the "dual" of m
-
-    for (int i = 0; i < k; i++) {
-        arr[i] = GetLargestV(a, b, x); //largest value v where v < a and vCb < x
-        x = x - Choose(arr[i], b);
-        a = arr[i];
-        b--;
-    }
-
-    for (int i = 0; i < k; i++) {
-        arr[i] = (n - 1) - arr[i];
-    }
-}
-
-/*
  * Returns largest value v where v < a and vCb <= x
  */
 int GetLargestV(int a, int b, int x)
@@ -302,3 +207,4 @@ int GetLargestV(int a, int b, int x)
     return v;
     
 }
+
